6250cce93abbfd67c158533ea9b9b83eb992c6eb
Kaushal <kaushal0181@gmail.com>
2018-10-25 02:43:13 +0530

    :butterfly: Flipkart IITK 2018

--- maxCoins.cpp
+++ maxCoins.cpp
@@ -17,60 +17,60 @@ using namespace std;
 
 vector<int> dummy(4);
 vector<int> greedy(int price, int p, int q, int r, int s) {
-	if (price <= p) return {price, 0, 0, 0};
-	price -= p;
+    if (price <= p) return {price, 0, 0, 0};
+    price -= p;
 
-	if (price <= 5 * q) {
-		if (price % 5 == 0) return {p, price / 5, 0, 0};
-		return dummy;
-	}
-	price -= 5 * q;
+    if (price <= 5 * q) {
+        if (price % 5 == 0) return {p, price / 5, 0, 0};
+        return dummy;
+    }
+    price -= 5 * q;
 
-	if (price <= 10 * r) {
-		if (price % 10 == 0) return {p, q, price / 10, 0};
-		return dummy;
-	}
-	price -= 10 * r;
+    if (price <= 10 * r) {
+        if (price % 10 == 0) return {p, q, price / 10, 0};
+        return dummy;
+    }
+    price -= 10 * r;
 
-	if (price <= 25 * s) {
-		if (price % 25 == 0) return {p, q, r, price / 25};
-		return dummy;
-	}
+    if (price <= 25 * s) {
+        if (price % 25 == 0) return {p, q, r, price / 25};
+        return dummy;
+    }
 
-	return dummy;
+    return dummy;
 }
 
 vector<int> getMaxCoins(int price, int p, int q, int r, int s) {
-	// Max wastage: 1.. 25; 5.. 5; 10.. 3
-	map<int, set<vector<int>>> mp;
-	for (int i = 0; i <= 25; i++) {
-		for (int j = 0; j <= 5; j++) {
-			for (int k = 0; k <= 3; k++) {
-				if (p >= i and q >= j and r >= k) {
-					auto counts = greedy(price, p - i, q - j, r - k, s);
-					if (counts != dummy) {
-						int count = 0;
-						for (int c : counts) {
-							count += c;
-						}
-						mp[-count].insert(counts);
-					}
-				}
-			}
-		}
-	}
-	if (mp.empty()) return dummy;
-	auto feasible = mp.begin()->second;
-	if (feasible.empty()) return dummy;
-	return *feasible.begin();
+    // Max wastage: 1.. 25; 5.. 5; 10.. 3
+    map<int, set<vector<int>>> mp;
+    for (int i = 0; i <= 25; i++) {
+        for (int j = 0; j <= 5; j++) {
+            for (int k = 0; k <= 3; k++) {
+                if (p >= i and q >= j and r >= k) {
+                    auto counts = greedy(price, p - i, q - j, r - k, s);
+                    if (counts != dummy) {
+                        int count = 0;
+                        for (int c : counts) {
+                            count += c;
+                        }
+                        mp[-count].insert(counts);
+                    }
+                }
+            }
+        }
+    }
+    if (mp.empty()) return dummy;
+    auto feasible = mp.begin()->second;
+    if (feasible.empty()) return dummy;
+    return *feasible.begin();
 }
 
 signed main() {
-	int denominations[] = {1, 5, 10, 25};
-	vector<int> counts = getMaxCoins(99, 98, 0, 0, 1);
-	
-	for (int i = 0; i < 4; i++) {
-		cout << "Number of coins of denomination " << denominations[i] << " = " << counts[i] << '\n';
-	}
-	return 0;
+    int denominations[] = {1, 5, 10, 25};
+    vector<int> counts = getMaxCoins(99, 98, 0, 0, 1);
+    
+    for (int i = 0; i < 4; i++) {
+        cout << "Number of coins of denomination " << denominations[i] << " = " << counts[i] << '\n';
+    }
+    return 0;
 }
--- minRectArea.cpp
+++ minRectArea.cpp
@@ -14,32 +14,32 @@ using namespace std;
 
 const int INF = 1E15;
 int getMinArea(vector<int> x, vector<int> y) {
-	map<pair<int,int>, set<int>> m;
-	int n = x.size();
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < i; j++) {
-			if (x[i] == x[j]) {
-				m[{min(y[i], y[j]), max(y[i], y[j])}].insert(x[i]);
-			}
-		}
-	}
-	
-	int ans = INF;
-	for (auto &p : m) {
-		int a = p.first.second - p.first.first;
-		vector<int> xs(p.second.begin(), p.second.end());
-		int sz = xs.size();
-		for (int i = 1; i < sz; i++) {
-			ans = min(ans, a * (xs[i] - xs[i - 1]));
-		}
-	}
-	return (ans < INF ? ans : -1);
+    map<pair<int,int>, set<int>> m;
+    int n = x.size();
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            if (x[i] == x[j]) {
+                m[{min(y[i], y[j]), max(y[i], y[j])}].insert(x[i]);
+            }
+        }
+    }
+    
+    int ans = INF;
+    for (auto &p : m) {
+        int a = p.first.second - p.first.first;
+        vector<int> xs(p.second.begin(), p.second.end());
+        int sz = xs.size();
+        for (int i = 1; i < sz; i++) {
+            ans = min(ans, a * (xs[i] - xs[i - 1]));
+        }
+    }
+    return (ans < INF ? ans : -1);
 }
 
 signed main() {
-	cout << getMinArea(
-		{1,1,2,2,3,3,4,4},
-		{1,3,1,8,1,8,1,3}
-	) << '\n';
-	return 0;
+    cout << getMinArea(
+        {1,1,2,2,3,3,4,4},
+        {1,3,1,8,1,8,1,3}
+    ) << '\n';
+    return 0;
 }
