72bb31ca41edc00d1655d488308293e9a4a8deed
Kaushal Agrawal <kaushal0181@gmail.com>
2019-10-09 15:12:36 +0530

    Update special binary string.cpp

--- special binary string.cpp
+++ special binary string.cpp
@@ -10,27 +10,21 @@ Given a special string S, a move consists of choosing two consecutive, non-empty
 
 At the end of any number of moves, what is the lexicographically largest resulting string possible?
 
+Solution :
+
+Since S is special, it must start with 1. If S is a string of 1s followed by 0s, we already have the lexicographically largest string possible.
+consider the prefixes of S in increasing length. First few prefixes will be all 1s.
+Now we will start having 1s followed by 0s. Call it type X.
+As we move further, now we have 1s followed by 0s followed by 1s. Call it type Y.
+
+algo : We will take smallest length prefix of type Y. Understand that it is simply a type X prefix followed by a 1. Our move will be to break the last 1 and attach at the beginning.
+Keep doing this and eventually you'll end up with type X string with length equal to that of S.
 */
 
-    string makeLargestSpecial(string S) {
-        int i=0, count=0;
-        vector<string> res;
-        for(int j=0;j<S.length();j++){
-            if(S[j]=='1')
-                count++;
-            else
-                count--;
-            if(count==0){
-                res.push_back('1' + makeLargestSpecial(S.substr(i+1,j-i-1)) +'0');
-                i = j+1;
-            }
-            
-        }
-        sort(res.begin(),res.end(),greater<string>());
-        
-        string r = "";
-        for(auto s:res){
-            r+=s;
-        }
-        return r;
-    }
+string makeLargestSpecial(string s) {
+    int n = s.size();
+    string t = "";
+    for (int i = 0; i < n / 2; i++) t += '1';
+    for (int i = 0; i < n / 2; i++) t += '0';
+    return t;
+}
