42790ebe8c2421ef47620f93fa54826413863d90
Kaushal Agrawal <kaushal0181@gmail.com>
2021-08-02 08:52:21 +0530

    Flipkart IITR 2018
    added by HarshBhagwani

--- consecutivePrimes.cpp
+++ consecutivePrimes.cpp
@@ -1,17 +1,19 @@
 /*
 IITR Flipkart 2018
 
-Given an array of N integers, return the length of maximum
+Given an array A of N integers, return the length of maximum
 subarray where all the elements are prime. It is known that
 the difference between the smallest and largest element of 
-the array is less than or equal to 1000000.
+the array is less than or equal to 10^6.
 
-1 <= N <= 100000
-1 <= elements <= 1000000000
+1 <= N <= 10^5
+1 <= A_i <= 10^9
+max{mod(A_i - A_j)} <= 10^6 for all 1 <= i, j <= N
 
 O(K * log K + N) where K is the difference between minimum
 and maximum element
 */
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -23,9 +25,7 @@ int consecutiveprimes(vector<int> a) {
 		if (mi > ai) mi = ai;
 		if (ma < ai) ma = ai;
 	}
-	cerr << "S1\tmi = " << mi << "\tma = " << ma << "\n";
 	vector<int> notp(ma - mi + 1);
-	// cerr << notp.size() << '\n';
 	for (int p = 2; p * p <= ma; p++) {
 		int i = max(2, mi / p) * p;
 		while (i < mi) i += p;
@@ -34,7 +34,6 @@ int consecutiveprimes(vector<int> a) {
 			i += p;
 		}
 	}
-	cerr << "S2\t" << notp.front() << notp.back() << "\n";
 	int cnt = 0, i = 0;
 	for (int &ai : a) {
 		assert(ai >= mi);
@@ -43,27 +42,21 @@ int consecutiveprimes(vector<int> a) {
 		if (ai) cnt++;
 		i++;
 	}
-	cerr << "S3\tcnt = " << cnt << "\n";
 	int consec = 0, cur = 0;
 	for (int &ai : a) {
 		if (not ai) cur = 0;
 		else consec = max(consec, ++cur);
 	}
-	cerr << "S4\n";
 	return consec;
 }
 
 
 int main() {
-	freopen("inp.txt", "r", stdin);
 	int n; cin >> n;
 	vector<int> a(n);
 	for (int i = 0; i < n; i++) {
 		cin >> a[i];
 	}
-	assert(a[n / 2] == 122846217 and n == 5E4);
-	cerr << "Read\n";
 	cout << consecutiveprimes(a) << '\n';
-	// Expected 30000
 	return 0;
 }
